a31eda4eed45432adbaecceb298b982219908bf6
TomK <tkistler@hsr.ch>
2020-06-24 15:23:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SeriesRenamer
+ Renames video files of a series consistent and adds the episode name.
---
+++ SeriesRenamer/FileNameRepresentation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Web;
+
+namespace SeriesRenamer
+{
+    class FileNameRepresentation
+    {
+
+        public FileNameRepresentation(string seriesName, int season, int episode, string episodeName)
+        {
+            Series = seriesName;
+            Season = season;
+            Episode = episode;
+            Name = episodeName;
+          
+        }
+
+        public static string Series { get; set; }
+        public int Season { get; set; }
+        public int Episode { get; set; }
+
+        private string _name;
+        public string Name {
+            get => HttpUtility.HtmlDecode(_name).Replace("ß", "ss");
+            set => _name = string.Concat(value.Split(Path.GetInvalidFileNameChars()));
+            
+        }
+
+        public string FullName { get => string.Format("{0} - {1:00}x{2:00} - {3}", Series, Season, Episode, Name); }
+    }
+}
