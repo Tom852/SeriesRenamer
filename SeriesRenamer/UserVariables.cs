008a42e4dd09ebc48ef51b09d70aa674a312825f
TomK <tkistler@hsr.ch>
2020-06-27 11:32:06 +0200

    UserVars non-static

--- SeriesRenamer/UserVariables.cs
+++ SeriesRenamer/UserVariables.cs
@@ -4,21 +4,19 @@ using System.Text;
 
 namespace SeriesRenamer
 {
-    public static class E
+    public class UserVariables
     {
-        public static string lang = "deutsch";
-        public static string folder = string.Empty;
-        public static string seriesName = string.Empty;
-        public static string wikiURL = string.Empty;
+        public string Lang { get; set; } = "deutsch";
+        public string Folder { get; set; } = string.Empty;
+        public string SeriesName { get; set; } = string.Empty;
+        public string WikiURL { get; set; } = string.Empty;
 
-        public static void Print()
+        public void Print()
         {
-
-                Console.WriteLine("INFO: Local Folder set to: " + folder);
-                Console.WriteLine("INFO: Series Name set to:  " + seriesName);
-                Console.WriteLine("INFO: Wiki URL set to:     " + wikiURL);
-                Console.WriteLine("INFO: Language set to:     " + lang);
-            
+                Console.WriteLine("INFO: Local Folder set to: " + Folder);
+                Console.WriteLine("INFO: Series Name set to:  " + SeriesName);
+                Console.WriteLine("INFO: Wiki URL set to:     " + WikiURL);
+                Console.WriteLine("INFO: Language set to:     " + Lang);
         }
     }
 }
